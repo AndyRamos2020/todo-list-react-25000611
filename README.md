7629d49ef59e0048fc8a641fbbd1dbe99e5e0266
AndyRamos <andy.ramos01gg@gmail.com>
2026-04-22 15:43:04 -0600

    proyecto react redux limpio

--- README.md
+++ README.md
@@ -1,67 +1,31 @@
-# To Do List - React Bootstrap
-
-## Descripción
-
-Aplicación web tipo To Do List para gestionar tareas y metas personales.
-Permite visualizar tareas con su respectiva fecha límite.
+To Do List - React Bootstrap
+Descripción
+Aplicación web tipo To Do List para gestionar tareas y metas personales. Permite visualizar tareas con su respectiva fecha límite.
 
 Nota: Esta versión corresponde únicamente a la maqueta (estructura visual), sin funcionalidad dinámica.
 
----
-
-## Tecnologías utilizadas
-
-* React
-* React Bootstrap
-* CSS
-
----
-
-## Funcionalidades (maqueta)
-
-* Campo para ingresar una tarea
-* Campo para seleccionar fecha
-* Botón para agregar tarea (visual)
-* Lista de tareas (estática)
-* Botón eliminar (visual)
-
----
-
-## Diseño Responsive
-
+Tecnologías utilizadas
+React
+React Bootstrap
+CSS
+Funcionalidades (maqueta)
+Campo para ingresar una tarea
+Campo para seleccionar fecha
+Botón para agregar tarea (visual)
+Lista de tareas (estática)
+Botón eliminar (visual)
+Diseño Responsive
 La aplicación se adapta a diferentes tamaños de pantalla gracias a React Bootstrap.
 
----
-
-## Cómo ejecutar el proyecto
-
-1. Clonar el repositorio:
-
-```
+Cómo ejecutar el proyecto
+Clonar el repositorio:
 git clone https://github.com/AndyRamos2020/todo-list-react-25000611.git
-```
-
-2. Instalar dependencias:
-
-```
+Instalar dependencias:
 npm install
-```
-
-3. Ejecutar el proyecto:
-
-```
+Ejecutar el proyecto:
 npm run dev
-```
-
----
-
-## Repositorio
-
+Repositorio
 GitHub: https://github.com/AndyRamos2020/todo-list-react-25000611
 
----
-
-## Autor
-
-Name: Andy David Ramos Jimenez
-Carnet: 25000611
+Autor
+Name: Andy David Ramos Jimenez Carnet: 25000611
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,45 @@
-import { Container, Row, Col } from "react-bootstrap";
-import Formulario from "./components/Form/Formulario";
-import Item from "./components/Item/item";
+import { Container, Row, Col, Navbar, Nav } from "react-bootstrap";
+import Formulario from "./components/form/Formulario";
+import Item from "./components/item/item";
 import "./App.scss";
 
 function App() {
   return (
-    <Container fluid className="main-container">
-      <Row>
+    <>
 
-        {/* FORMULARIO */}
-        <Col md={6}>
-          <Formulario />
-        </Col>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand>React-Bootstrap</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#">Tareas</Nav.Link>
+            <Nav.Link href="#">Metas</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
 
-        {/* TARJETAS */}
-        <Col md={6} className="cards-section">
-          <Item 
-            title="Meta 1"
-            description="Descripción 1"
-            date="15/05/2026"
-          />
 
-          <Item 
-            title="Meta 2"
-            description="Otra meta importante"
-            date="20/05/2026"
-          />
-        </Col>
+      <Container fluid className="main-container">
+        <Row>
+          <Col md={6}>
+            <Formulario />
+          </Col>
 
-      </Row>
-    </Container>
+          <Col md={6} className="cards-section">
+            <Item 
+              title="Meta 1"
+              description="Descripción 1"
+              date="15/05/2026"
+            />
+
+            <Item 
+              title="Meta 2"
+              description="Otra meta importante"
+              date="20/05/2026"
+            />
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
