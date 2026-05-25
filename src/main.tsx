9f115bb9d2135b6d7962557aa09b020912163bb7
AndyRamos <andy.ramos01gg@gmail.com>
2026-05-25 13:17:02 -0600

    proyecto final

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './styles/main.scss'
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
-import { Provider } from 'react-redux'
-// @ts-expect-error: module has no declaration file
-import { store } from './redux/store'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(
+  document.getElementById("root")!
+).render(
   <Provider store={store}>
     <App />
   </Provider>
-)
+);
